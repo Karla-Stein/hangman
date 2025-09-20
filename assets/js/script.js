dc4b513ebd5d3c2cf88fbdfedc63460d14604b3a
k_stein <karlasteinbrink@gmail.com>
2025-09-20 21:21:05 +0100

    Add button section and styling

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",function(){
+    let alphabetArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    for (let char of alphabetArray){
+       let button =  document.createElement("button");
+       button.classList.add("btn", "btn-light", "btn-lg");
+       button.innerText = char;
+       document.getElementById("buttons-1").appendChild(button);  
+    }   
+}
+)
