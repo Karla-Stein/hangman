79aaa209fd80c4cdc06fa2f6558a0c7b18484a97
k_stein <karlasteinbrink@gmail.com>
2025-09-20 21:26:01 +0100

    Label sections and add docstring to function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Creates Alphabet buttons and adds them to the DOM 
+ */
 document.addEventListener("DOMContentLoaded",function(){
     let alphabetArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     for (let char of alphabetArray){
